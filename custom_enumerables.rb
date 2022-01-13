d1055d952406c56c92354a9aa520696ab4f247c0
Jeroen B <jeroenb04@live.com>
2022-01-13 21:20:10 +0100

    Solved #none?

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -19,13 +19,24 @@ module Enumerable
   end
 
   def my_any?(tester='')
-    if tester == ''
+    if block_given?
       my_each { |t| return true if yield t}
       false
     else
       my_each { |t| return true if tester === t }
       false
     end
+  end
+
+  def my_none?(tester = '')
+    if block_given?
+      my_each { |t| return false if yield t}
+    elsif tester != ''
+      my_each { |t| return false if tester === t }
+    elsif my_any? { |t| t == true}
+      return false
+    end
+    true
   end
 end
 
@@ -55,14 +66,20 @@ end
 # p %w[ant bear cat].my_all? { |word| word.length >= 4 }
 
 # Any?
-p %w[ant bear cat].any? { |word| word.length >= 3 }
-p %w[ant bear cat].my_any? { |word| word.length >= 3 }
-p %w[ant bear cat].any?(/d/)
-p %w[ant bear cat].my_any?(/d/)
-p [].any?
-p [].my_any?
+# p %w[ant bear cat].any? { |word| word.length >= 3 }
+# p %w[ant bear cat].my_any? { |word| word.length >= 3 }
+# p %w[ant bear cat].any?(/d/)
+# p %w[ant bear cat].my_any?(/d/)
+# p [].any?
+# p [].my_any?
 
 # None?
+p %w{ant bear cat}.none? { |word| word.length == 5 }
+p %w{ant bear cat}.none?(/d/) 
+p [nil, false, true].none?
+p %w{ant bear cat}.my_none? { |word| word.length == 5 }
+p %w{ant bear cat}.my_none?(/d/) 
+p [nil, false, true].my_none?
 
 # Count
 
