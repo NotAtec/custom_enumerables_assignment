8f961038b8515e42b2a3db3e9a3f22b363760c06
Jeroen B <jeroenb04@live.com>
2022-01-13 20:42:50 +0100

    Solved #each

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,5 +1,12 @@
 module Enumerable
   def my_each
-
+    len = self.length
+    len.times { |i| yield self[i] }
+    yield # Numbers value [index]
   end
 end
+
+puts "my_each vs. each"
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each  { |item| puts item }
+numbers.each  { |item| puts item }
